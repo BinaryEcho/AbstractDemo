82a8c32090e261091805924c039b195947441643
Varun Bharadwaj <vkb.mech@gmail.com>
2019-01-09 14:34:37 -0600

    Update Kumar.java

--- src/Kumar.java
+++ src/Kumar.java
@@ -1,11 +1,12 @@
 public class Kumar extends Man {
-    public Kumar(){
+    public Kumar(String iHusband){
+        super(iHusband);
     }
     public void setFamily(){
         setWife("Madhu");
         setChild("Taashvi");
     }
     public void customMessage(){
-        System.out.println("Welcome " + getHusband() + "! Hope " + getWife() + " and " + getChild() + " are doing well.  Let's setup a lunch outing soon!");
+        System.out.println("Welcome " + getHusband() + "! Hope " + getWife() + " and " + getChild() + " are doing well. Let's setup another lunch outing soon!");
     }
 }
