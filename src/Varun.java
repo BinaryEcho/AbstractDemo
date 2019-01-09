ac84b26da439a343797db23bf89b8d5bda6fbfea
Varun Bharadwaj <vkb.mech@gmail.com>
2019-01-09 14:35:20 -0600

    Update Varun.java

--- src/Varun.java
+++ src/Varun.java
@@ -1,12 +1,12 @@
 public class Varun extends Man {
-    public Varun(){
-        setHusband();
+    public Varun(String iHusband){
+        super(iHusband);
     }
     public void setFamily(){
         setWife("Kruthi");
         setChild("");
     }
     public void customMessage(){
-        System.out.println("Welcome " + getHusband() + "! No custom message for you. Sorry (Your wife is " + getWife() + ", by the way");
+        System.out.println("Welcome " + getHusband() + "! No custom message for you. Sorry (Your wife is " + getWife() + ", by the way)");
     }
 }
